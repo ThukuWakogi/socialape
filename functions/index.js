04e756c6aea081fc3cad15d071aeef562b97800c
Timothy Oliver <oliverwakogi@live.com>
2019-07-06 22:14:42 +0300

    Getting and commenting on scream

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,9 @@ const functions = require('firebase-functions')
 const app = require('express')()
 const { 
   getAllScreams, 
-  postOneScream 
+  postOneScream,
+  getScream,
+  commentOnScream
 } = require('./handlers/Screams')
 const { 
   signUp, 
@@ -15,6 +17,11 @@ const FireBaseAuth = require('./util/FirebaseAuth')
 
 app.get('/screams', getAllScreams)
 app.post('/scream', FireBaseAuth, postOneScream)
+app.get('/scream/:screamId', getScream)
+// TODO: delete scream
+// TODO: like a scream
+// TODO: unlike a scream
+app.post('/scream/:screamId/comment', FireBaseAuth, commentOnScream)
 app.post('/signup', signUp)
 app.post('/login', login)
 app.post('/user/image', FireBaseAuth, uploadImage)
